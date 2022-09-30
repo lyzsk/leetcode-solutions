5c8d9d0a3b8332a9eded48125160d46d078747ab
lyzsk <sichu.huang2021@outlook.com>
2022-09-30 17:52:56 +0100

    add java solution for leetcode0033

---
+++ java-solutions/0033-search-in-rotated-sorted-array/src/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * @author sichu
+ * @date 2022/09/30
+ **/
+public class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[nums.length - 1]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
