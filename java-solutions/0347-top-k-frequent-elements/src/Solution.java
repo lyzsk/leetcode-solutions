f6840f385ff1ee0d0251bb96f3b1fa54fd5e8622
lyzsk <sichu.huang2021@outlook.com>
2022-10-19 16:49:47 +0100

    update Java solutions for leetcode0347

--- java-solutions/0347-top-k-frequent-elements/src/Solution.java
+++ java-solutions/0347-top-k-frequent-elements/src/Solution.java
@@ -9,10 +9,13 @@ import java.util.PriorityQueue;
  **/
 public class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        // 方法一
         // Map<Integer, Integer> freq = new HashMap<>(16);
         // Arrays.stream(nums).forEach(num -> freq.put(num, freq.getOrDefault(num, 0) + 1));
         // return freq.entrySet().stream().sorted((o1, o2) -> o2.getValue() - o1.getValue()).limit(k)
         //     .mapToInt(Map.Entry::getKey).toArray();
+
+        // 方法二
         Map<Integer, Integer> freq = new HashMap<>(16);
         for (int num : nums) {
             freq.put(num, freq.getOrDefault(num, 0) + 1);
@@ -36,5 +39,23 @@ public class Solution {
             res[i] = pq.poll()[0];
         }
         return res;
+        
+        // 方法三
+        // Map<Integer, Integer> freq = new HashMap<>(16);
+        // for (int num : nums) {
+        //     freq.put(num, freq.getOrDefault(num, 0) + 1);
+        // }
+        // PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((o1, o2) -> o1.getValue() - o2.getValue());
+        // for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+        //     pq.offer(entry);
+        //     if (pq.size() > k) {
+        //         pq.poll();
+        //     }
+        // }
+        // int[] res = new int[k];
+        // for (int i = 0; i < k; i++) {
+        //     res[i] = pq.poll().getKey();
+        // }
+        // return res;
     }
 }
