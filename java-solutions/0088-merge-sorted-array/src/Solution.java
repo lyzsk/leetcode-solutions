81ac46896813b6153861f11374b62ada4fa1d7fe
lyzsk <sichu.huang2021@outlook.com>
2023-05-02 16:12:34 +0800

    update java solution for leetcode 0088

--- java-solutions/0088-merge-sorted-array/src/Solution.java
+++ java-solutions/0088-merge-sorted-array/src/Solution.java
@@ -9,21 +9,13 @@ public class Solution {
         int cur = m + n - 1;
         while (i >= 0 || j >= 0) {
             if (i == -1) {
-                nums1[cur] = nums2[j];
-                --cur;
-                --j;
+                nums1[cur--] = nums2[j--];
             } else if (j == -1) {
-                nums1[cur] = nums1[i];
-                --cur;
-                --i;
+                nums1[cur--] = nums1[i--];
             } else if (nums1[i] > nums2[j]) {
-                nums1[cur] = nums1[i];
-                --cur;
-                --i;
+                nums1[cur--] = nums1[i--];
             } else {
-                nums1[cur] = nums2[j];
-                --cur;
-                --j;
+                nums1[cur--] = nums2[j--];
             }
         }
     }
