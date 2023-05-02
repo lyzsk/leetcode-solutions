d3ae648294498f590ccdc4244c9b0e2ec223167f
lyzsk <sichu.huang2021@outlook.com>
2023-05-02 16:54:44 +0800

    update java solution for leetcode 0027

--- java-solutions/0027-remove-element/src/Solution.java
+++ java-solutions/0027-remove-element/src/Solution.java
@@ -4,20 +4,11 @@
  **/
 public class Solution {
     public int removeElement(int[] nums, int val) {
-        // int n = nums.length;
-        // int left = 0;
-        // for (int right = 0; right < n; right++) {
-        //     if (nums[right] != val) {
-        //         nums[left++] = nums[right];
-        //     }
-        // }
-        // return left;
         int left = 0;
-        int right = nums.length;
-        while (left < right) {
+        int right = nums.length - 1;
+        while (left <= right) {
             if (nums[left] == val) {
-                nums[left] = nums[right - 1];
-                --right;
+                nums[left] = nums[right--];
             } else {
                 ++left;
             }
