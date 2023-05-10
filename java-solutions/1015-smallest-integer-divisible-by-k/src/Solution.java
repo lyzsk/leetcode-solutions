aa9e042566fb5931247ad8376b38ef55c39a4432
lyzsk <sichu.huang2021@outlook.com>
2023-05-10 16:04:49 +0800

    update java solution for leetcode 1015

--- java-solutions/1015-smallest-integer-divisible-by-k/src/Solution.java
+++ java-solutions/1015-smallest-integer-divisible-by-k/src/Solution.java
@@ -1,35 +1,32 @@
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * @author sichu
  * @date 2023/05/10
  **/
 public class Solution {
     public int smallestRepunitDivByK(int k) {
-        // if (k % 2 == 0 || k % 5 == 0) {
-        //     return -1;
-        // }
-        // int res = 1;
-        // int resid = 1 % k;
-        // while (resid != 0) {
-        //     resid = (resid * 10 + 1) % k;
-        //     ++res;
-        // }
-        // return res;
-
+        if (k % 2 == 0 || k % 5 == 0) {
+            return -1;
+        }
         int res = 1;
         int resid = 1 % k;
-        Set<Integer> set = new HashSet<>();
-        set.add(resid);
         while (resid != 0) {
             resid = (resid * 10 + 1) % k;
             ++res;
-            if (set.contains(resid)) {
-                return -1;
-            }
-            set.add(resid);
         }
         return res;
+
+        // int res = 1;
+        // int resid = 1 % k;
+        // Set<Integer> set = new HashSet<>();
+        // set.add(resid);
+        // while (resid != 0) {
+        //     resid = (resid * 10 + 1) % k;
+        //     ++res;
+        //     if (set.contains(resid)) {
+        //         return -1;
+        //     }
+        //     set.add(resid);
+        // }
+        // return res;
     }
 }
