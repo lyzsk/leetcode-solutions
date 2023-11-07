a690c6ed83b083fc20bed7d59b6238fb2d06f44c
lyzsk <sichu.huang2021@outlook.com>
2023-11-08 00:27:47 +0800

    update java solution for leetcode 1921

--- java-solutions/1921-eliminate-maximum-number-of-monsters/src/Solution.java
+++ java-solutions/1921-eliminate-maximum-number-of-monsters/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * @author sichu
  * @date 2023/11/07
@@ -8,17 +10,10 @@ public class Solution {
         int[] arrivalTimes = new int[n];
         for (int i = 0; i < n; i++) {
             arrivalTimes[i] = (int)Math.ceil((double)dist[i] / speed[i]);
-            speed[i] = 0;
         }
-        for (int arrivalTime : arrivalTimes) {
-            if (arrivalTime >= n) {
-                continue;
-            }
-            ++speed[arrivalTime];
-        }
-        for (int i = 1; i < n; i++) {
-            speed[i] += speed[i - 1];
-            if (speed[i] > i) {
+        Arrays.sort(arrivalTimes);
+        for (int i = 0; i < n; i++) {
+            if (arrivalTimes[i] <= i) {
                 return i;
             }
         }
