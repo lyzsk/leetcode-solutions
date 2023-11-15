0a985cfa4c38515f3bb5cd26f6012bb2ff84339f
lyzsk <sichu.huang2021@outlook.com>
2023-11-15 13:28:05 +0800

    add java solution for leetcode 1164

--- sql-solutions/1164-product-price-at-a-given-date/solution.sql
+++ sql-solutions/1164-product-price-at-a-given-date/solution.sql
@@ -9,7 +9,7 @@ where product_id not in (
 union
 select product_id, new_price as price
 from products
-where product_id, change_date
+where (product_id, change_date)
 in (
     select product_id, max(change_date) as date
     from products
