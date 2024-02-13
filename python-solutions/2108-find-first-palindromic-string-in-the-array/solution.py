ca5c9030ff89f5cefc07ab1cb5f4549724529f69
lyzsk <sichu.huang2021@outlook.com>
2024-02-13 15:16:36 +0800

    update py solution for leetcode 2108

--- python-solutions/2108-find-first-palindromic-string-in-the-array/solution.py
+++ python-solutions/2108-find-first-palindromic-string-in-the-array/solution.py
@@ -6,18 +6,6 @@ class Solution:
         if words is None or len(words) == 0:
             return None
         for word in words:
-            if self.is_palidrome(word):
+            if word == word[::-1]:
                 return word
         return ""
-
-    def is_palidrome(self, word: str) -> bool:
-        if word is None or len(word) == 0:
-            return False
-        left = 0
-        right = len(word) - 1
-        while left < right:
-            if word[left] != word[right]:
-                return False
-            left += 1
-            right -= 1
-        return True
